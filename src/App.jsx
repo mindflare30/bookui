292643145ee56ac31783f63db3a88dbb14e2459a
mindflare30 <140968326+mindflare30@users.noreply.github.com>
2024-06-21 22:41:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./home/Home";
-import {  Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate  } from "react-router-dom";
 import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
