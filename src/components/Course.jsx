2d70eeff8519769ad40058ca480928aee8a8f5be
unknown <you@example.com>
2024-06-21 22:03:37 +0530

    first commit

---
+++ src/components/Course.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("https://bookserver-zh6l.onrender.com/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            Discover your next{" "}
+            <span className="text-pink-500"> Read! :)</span>
+          </h1>
+          <p className="mt-12">
+            Delve into stories that inspire, educate, and entertain, each page 
+            offering a new adventure. Whether you're seeking knowledge, escape,
+             or a new perspective, the right book can open doors to limitless possibilities.
+              Immerse yourself in the magic of words and let your imagination soar. 
+              Find a book that resonates with you, and let it be a companion on your path to growth and discovery.
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
---
+++ src/components/Freebook.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+import axios from "axios";
+
+import Cards from "./Cards";
+function Freebook() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("https://bookserver-zh6l.onrender.com/book");
+
+        const data = res.data.filter((data) => data.category === "Free");
+        console.log(data);
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+          <p>
+            Ever lusted over a book but had to control yourself because it was too expensive?
+            Worry not! Because Bookhaven is here to answer all your literary prayers.
+            Knowledge is priceless but the books come with a bill and the Bookhaven is
+            committed to bring to you the best of the brilliant from the world of written text
+            , at prices which are literally a steal.
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Freebook;
